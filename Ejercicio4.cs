e6cb6e926a82e33f075c748af2f6362085a74478
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-05-15 11:41:26 -0300

    Update Ejercicio4.cs

--- Ejercicio4.cs
+++ Ejercicio4.cs
@@ -1,1 +1,27 @@
-//aaaa
+double[] array = {5,7,8,4,3,2,6,9}; //pq no tengo creatividad para hacer numeros nuevos :)
+
+ foreach(double numero in array)
+        {
+            Console.Write(numero + " ");
+        }
+        Console.WriteLine();
+
+
+ foreach(double numero in multiplicarPorPi(array))
+        {
+            Console.Write(numero + " ");
+        }
+
+
+double[] multiplicarPorPi(double[] nums)
+{
+    double[] resultado = new double[nums.Length];
+
+    for(int i = 0; i < nums.Length; i++)
+    {
+     resultado[i] = nums[i] * 3.14;
+    }
+
+    return resultado;
+    
+}
