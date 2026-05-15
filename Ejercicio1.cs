38479ddae6b886dd5b3013d3c6ec317e878e851d
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-05-15 11:04:42 -0300

    Implement array merging based on even/odd values
    Added two integer arrays and a method to merge them based on even/odd conditions.

--- Ejercicio1.cs
+++ Ejercicio1.cs
@@ -1,1 +1,33 @@
-//aaaaaaaaa
+int[] nums1 = {1,2,3,4,5,6,7,8,9,10};
+int[] nums2 = {11,12,13,14,15,16,17,18,19,20};
+
+Console.WriteLine("Arreglo 1:");
+foreach(int num in nums1)
+{
+    Console.Write(num + " ");
+}
+
+Console.WriteLine("\nArreglo 2:");
+foreach(int num in nums2)
+{
+    Console.Write(num + " ");
+}
+
+Console.WriteLine("\nResultado:");
+foreach(int num in mezclarArreglos(nums1,nums2))
+{
+    Console.Write(num + " ");
+}
+
+int[] mezclarArreglos(int[] arreglo1, int[] arreglo2)
+{
+    int[] resultado = new int[10];
+
+    for(int i = 0; i < arreglo1.Length; i++)
+    {
+        if(arreglo2[i] % 2 == 0) resultado[i] = arreglo2[i];
+        else resultado[i] = arreglo1[i];
+    }
+    return resultado;
+    
+}
