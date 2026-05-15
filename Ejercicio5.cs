e04f7f1926d89ec386ed4c6b1c931285f2537e75
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-05-15 11:58:16 -0300

    Update Ejercicio5.cs

--- Ejercicio5.cs
+++ Ejercicio5.cs
@@ -1,1 +1,15 @@
-//aaaa
+int[] array = {5,7,8,4,3,2,6,9}; //pq no tengo creatividad para hacer numeros nuevos :)
+
+Console.WriteLine("Ingrese numero a buscar");
+int.TryParse(Console.ReadLine().Trim(),out int num);
+Console.WriteLine(busquedaLineal(array,num));
+
+int busquedaLineal(int[] arreglo, int valor)
+{
+    for(int i = 0; i < arreglo.Length; i++)
+    {
+        if(valor == arreglo[i]) return i;  //Si encuentra una coincidencia,devuelve el slot
+    }
+
+    return -1; // Si cierra el ciclo,no encontro nada y mandamos -1
+}
