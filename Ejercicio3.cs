801c84009b3b12d70c60cb392afb4bd58153f4c6
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-05-15 11:22:30 -0300

    Update Ejercicio3.cs

--- Ejercicio3.cs
+++ Ejercicio3.cs
@@ -1,1 +1,25 @@
-//aaaa
+int[] array = {5,7,8,4,3,2,6,9};
+
+foreach(int num in array)
+{
+    Console.Write(num + " ");
+}
+Console.WriteLine();
+
+foreach(int num in reverso(array))
+{
+    Console.Write(num + " ");
+}
+
+int[] reverso(int[] nums)
+{
+    int[] resultado = new int[nums.Length];
+    
+    for(int i = 0; i < nums.Length; i++)
+    {
+        resultado[i] = nums[nums.Length - (i + 1)];
+
+    } //N - (i + 1)
+
+    return resultado;
+}
