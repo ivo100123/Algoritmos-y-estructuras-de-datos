060263d098f8a64b2e6e029e90c3d6f2f361b4c5
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-05-29 10:08:07 -0300

    Update Ejercicio4.cs

--- Ejercicio4.cs
+++ Ejercicio4.cs
@@ -1,17 +1,17 @@
-int[] array = {1,2,3,4,5,6,7,8,9};
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 Console.WriteLine("Ingrese numero a buscar");
-int.TryParse(Console.ReadLine().Trim(),out int num);
-Console.WriteLine(busquedaLineal(array,num));
+int.TryParse(Console.ReadLine().Trim(), out int num);
+Console.WriteLine("La posicion es" + busquedaLineal(array, num));
 
 int busquedaLineal(int[] arreglo, int valor)
 {
-    
-    for(int i = 0; i < arreglo.Length; i++)
+
+    for (int i = 0; i < arreglo.Length; i++)
     {
-        if(array[i] == valor) return i;
+        if (array[i] == valor) return i;
         else i = valor - 2;
     }
 
-    return -1; 
+    return -1;
 }
