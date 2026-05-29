fa65b27f2c57d23fa91dc5551e68b83017ee3eed
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-05-29 09:17:26 -0300

    Update Ejercicio2.cs

--- Ejercicio2.cs
+++ Ejercicio2.cs
@@ -1,1 +1,1 @@
-// Este ejercicio estaba repetido en el link del classroom. Revisar en rama Arreglos-en-C#
+// Este ejercicio estaba repetido en el link del classroom. Revisar ejercicio 5 en rama Arreglos-en-C#
