32692fed0d990022e72c3b7dac51856be9f53249
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-05-29 09:32:13 -0300

    Implement number insertion into sorted array
    Add functionality to insert a number into an array in sorted order.

--- Ejercicio5.cs
+++ Ejercicio5.cs
@@ -1,1 +1,24 @@
 
+int[] array = {1 , 2, 3 , 4 , 5 , 0};
+
+Console.WriteLine("Inserte el numero a ingresar");
+int.TryParse(Console.ReadLine(),out int num);
+
+foreach(int componente in intercalar(array,num))
+{
+    Console.Write(componente + " ");
+}
+
+int[] intercalar(int[] a, int valor)
+{
+    int i; //la pongo afuera para poder usarla fuera del for 
+
+    for(i = a.Length - 2; i >= 0 && a[i] > valor; i--)
+    {
+        a[i + 1] = a[i];
+    }
+
+    a[i + 1] = valor; 
+
+    return a;
+}
