b59d4d68ece59ebe0a4e38d0b64ea2a8ea76f571
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-06-19 09:24:57 -0300

    Create Ejercicio6.cs

---
+++ Ejercicio6.cs
@@ -0,0 +1,24 @@
+int[,] matriz = new int[3, 3] 
+{
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 }
+};
+
+int mayorNum = 0;
+int mayorColumna = 0;
+
+for(int i = 0; i < matriz.GetLength(0); i++) 
+{
+    for(int j = 0; j < matriz.GetLength(1); j++) 
+    {
+        if (matriz[i,j] > mayorNum) 
+        {
+         mayorNum = matriz[i,j];
+         mayorColumna = j;
+        }
+    }
+   
+} 
+
+Console.WriteLine($"La columna con el mayor es la fila {mayorColumna}");
