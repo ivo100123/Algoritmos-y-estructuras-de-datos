2c3d428dff5426441225c72a1e76ee9710fd5f61
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-06-19 08:29:51 -0300

    Create Ejercicio1.cs

---
+++ Ejercicio1.cs
@@ -0,0 +1,20 @@
+int[,] matriz = new int[3, 3] 
+{
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 }
+};
+
+int cont = 0;
+
+for(int i = 0; i < matriz.GetLength(0); i++) 
+{
+    
+    for(int j = 0; j < matriz.GetLength(1); j++) 
+    {
+        cont += matriz[i, j];
+    }
+} 
+
+
+Console.WriteLine($"Su resultado es {cont}");
