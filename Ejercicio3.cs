a2075d9f0c643ee58d5754f0f99a8c59400e1237
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-06-19 08:41:36 -0300

    Create Ejercicio3.cs

---
+++ Ejercicio3.cs
@@ -0,0 +1,21 @@
+int[,] matriz = new int[3, 3] 
+{
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 }
+};
+
+int mayor = 0;
+
+Console.WriteLine("La matriz es de 3x3. ¿Que fila quiere revisar? 0 - 2");
+int.TryParse(Console.ReadLine().Trim(),out int input);
+
+for(int i = 0; i < matriz.GetLength(1); i++) 
+{
+    
+    if (matriz[input, i] > mayor) mayor = matriz[input, i];
+
+
+} 
+
+Console.WriteLine($"El mayor numero de la fila es {mayor}");
