343cbb0480dfce080a886b979bac45b7195a22b9
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-06-26 09:31:38 -0300

    Implement matrix multiplication and print results
    This code multiplies two 2x2 matrices A and B, storing the result in matrix C. It then prints the elements of matrix C.

---
+++ Ejercicio17.cs
@@ -0,0 +1,29 @@
+double[,] A = new double[2,2]
+{
+    {1.5,2.3},
+    {4.2,5.2}
+};
+
+double[,] B = new double[2,2]
+{
+    {2.3,5.3},
+    {8.4,7.4}
+};
+
+double[,] C = new double[2,2];
+
+for(int i = 0; i < A.GetLength(0); i++)
+{
+    for(int j = 0; j < A.GetLength(1); j++)
+    {
+        C[i,j] = A[i,j] * B[i,j];
+    }
+}
+
+for(int i = 0; i < A.GetLength(0); i++)
+{
+    for(int j = 0; j < A.GetLength(1); j++)
+    {
+        Console.WriteLine(C[i,j] + " ");
+    }
+}
