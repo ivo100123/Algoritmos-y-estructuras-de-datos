a6530be3f065c7763c72ca9048897e95c90fc67f
ivo100123 <167706027+ivo100123@users.noreply.github.com>
2026-08-24 11:16:26 -0300

    Create Ejercicio3.cs

---
+++ Ejercicio3.cs
@@ -0,0 +1,9 @@
+/*Objetivo: Trabajar con diferentes métodos de lectura de StreamReader (ReadToEnd y Peek).
+Consigna
+1. Crea un programa que lea el archivo diario.txt generado en el Ejercicio 1.
+2. Utiliza sr.Peek() para comprobar si el archivo tiene contenido antes de comenzar a leer.
+3. Utiliza sr.ReadToEnd() para obtener todo el contenido en un solo string.
+4. Muestra en consola:
+○ Todo el contenido del archivo.
+○ La cantidad total de caracteres.
+○ La cantidad total de palabras.*/
